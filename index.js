524b7731d2a42884cca48d451759318b46095e42
xiaonanguitar <zhangzhinan1987@126.com>
2017-09-13 00:03:11 +0800

    delete render method

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 //Created by z00250978 on 2017/03/24.
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import components from './src/components';
+/*
+const {Collapse} = components;
+const Panel = Collapse.Panel;
+
+class App extends Component {
+    
+        constructor(props,context) {
+            super(props)
+        }
+    
+        render() {
+            return (
+                <Collapse>
+                    <Panel>
+                        <div>xxxx</div>
+                    </Panel>
+                    <Panel>
+                    <div>xxxx</div>
+                    <div>xxxx</div>
+                    <div>xxxx</div>
+                    <div>xxxx</div>
+                    </Panel>
+                </Collapse>
+            )
+        }
+    }
 
 
 ReactDOM.render(<App/>,
   document.getElementById('content')
 );
-
+*/
 
 module.exports = components;
 
